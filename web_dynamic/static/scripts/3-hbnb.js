3ae4ae510c838cecf5218282909d1949e66a5e22
teamkweku <teamkweku@outlook.com>
2023-11-06 15:20:19 +0000

    implementation for fetching places using ajax call

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,50 @@
+/* script that checks if a checkbox is checked */
+$(document).ready(function () {
+  const amenitiesId = {};
+  $('INPUT[type="checkbox"]').click(function () {
+    if ($(this).prop('checked')) {
+      amenitiesId[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenitiesId[$(this).attr('data-id')];
+    }
+    $('.amenities h4').text(Object.values(amenitiesId).join(', '));
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status/', data => {
+    if (data.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+
+  $.ajax({
+    url: `http://0.0.0.0:5001//api/v1/places_search`,
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify({}),
+    success: function (data) {
+      for (let place of data) {
+        let article = `
+        <article>
+        <div class="title_box">
+        <h2>${place.name}</h2>
+        <div class="price_by_night">$${place.price_by_night}</div>
+        </div>
+        <div class="information">
+        <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? "s" : ""
+          }</div>
+        <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? "s" : ""
+          }</div>
+        <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? "s" : ""
+          }</div>
+        </div> 
+        <div class="description">
+        ${place.description}
+        </div>
+          </article>
+        `;
+        $('section.places').append(article);
+      }
+    }
+  });
+});
